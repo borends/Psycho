e2d5e833f46f13f9810c5a5b60a9076a3da7089b
borends <113675265+borends@users.noreply.github.com>
2024-03-30 20:34:01 +0700

    Alpha 0.00.05
    Alpha 0.00.05

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform SpawnPoint;
+
+    void Start()
+    {
+        Invoke("Spawn", 5);
+    }
+
+    public void Spawn()
+    {
+        Instantiate(enemy, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("Winning", 120);
+    }
+
+    private void Winning()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
